5fb28fae1889e9e606700b0257c0d52924a5aef5
Sanuja Rubasinghe <sanurubasinghe1234@gmail.com>
2025-05-03 16:31:24 +0530

    set base api url

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 const API = axios.create({
-    baseURL: `${process.env.REACT_APP_API_URL}/api/`,
+    baseURL: 'https://test-hardware-backend.onrender.com/api/',
     withCredentials: true
 })
 
