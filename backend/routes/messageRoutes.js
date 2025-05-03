119b12734bb95b7d1f624f2f142a189b38adf29b
Sanuja Rubasinghe <sanurubasinghe1234@gmail.com>
2025-05-03 13:50:55 +0530

    Untrack all files and re-add based on .gitignore

--- backend/routes/messageRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require('express')
-const router = express.Router()
-const {sendWhatsAppMessage} = require('../controllers/messageController')
-
-router.post('/send-message', sendWhatsAppMessage)
-
-module.exports = router
--- backend/routes/userLocationRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require('express')
-const router = express.Router()
-const {getDeliveryCharges} = require("../controllers/userLocationController")
-
-router.post('/delivery-charges', getDeliveryCharges)
-
-module.exports = router
